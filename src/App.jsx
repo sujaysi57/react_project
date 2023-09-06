74d94f0ee502e080d7cfd1f943a3c077428f208d
Sujay <sujayingale@gmail.com>
2023-09-06 21:02:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NewProj from "./newProj";
+
+function App() {
+  return (
+    <>
+      <NewProj />
+      <h1>This is next line of code</h1>
+      <p>This Next paragraph</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/newProj.jsx
@@ -0,0 +1,5 @@
+function NewProj() {
+  return <h3>React project is ready</h3>;
+}
+
+export default NewProj;
